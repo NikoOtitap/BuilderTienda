5b91d5572ffcd0dcfe6f729775c4f38c1d944382
NikoOtitap <nicotitap@mail>
2026-09-07 20:43:45 -0500

    feat: minor change in client

--- src/co/edu/udistrital/app/Cliente.java
+++ src/co/edu/udistrital/app/Cliente.java
@@ -25,75 +25,72 @@ import java.util.Arrays;
 public class Cliente {
 
     private PromocionDirector director;
-
-    private Celular celular;
-    private Audifono audifono;
-    private Mouse mouse;
-    private Laptop laptop;
-    private Funda funda;
-    private MemoriaUsb usb;
-
-    private Promocion promo1;
-    private Promocion promo2;
-    private Promocion promo3;
+    private Promocion promo2x1;
+    private Promocion promoVolumen;
+    private Promocion promoCombo;
 
     public Cliente() {
         this.director = new PromocionDirector();
-        this.celular = null;
-        this.audifono = null;
-        this.mouse = null;
-        this.laptop = null;
-        this.funda = null;
-        this.usb = null;
-        this.promo1 = null;
-        this.promo2 = null;
-        this.promo3 = null;
+        this.promo2x1 = null;
+        this.promoVolumen = null;
+        this.promoCombo = null;
     }
 
     public void ejecutar() {
-        configurarProductos();
         crearPromocion2x1();
         crearPromocionVolumen();
         crearPromocionCombo();
-        mostrarInformacion();
-    }
 
-    private void configurarProductos() {
-        celular = new Celular("Galaxy A54", 800000, "Exynos", 8, 128, "Android");
-        audifono = new Audifono("AirBuds", 150000, true, 20);
-        mouse = new Mouse("Logitech M170", 40000, true);
-        laptop = new Laptop("ThinkPad E14", 3000000, "i5", 16, 512, "Windows", null);
-        funda = new Funda("Funda 14 pulgadas", 50000, "Laptop");
-        usb = new MemoriaUsb("Kingston 64GB", 30000, 64);
+        mostrarInfo2x1();
+        mostrarInfoVolumen();
+        mostrarInfoCombo();
     }
 
     private void crearPromocion2x1() {
+        Celular celular = new Celular("Galaxy A54", 800000, "Exynos", 8, 128, "Android");
+        Audifono audifono = new Audifono("AirBuds", 150000, true, 20);
+
         PromocionBuilder builder = new Promocion2x1Builder(celular, audifono);
-        promo1 = director.build(builder, "Celular + Audifonos 2x1");
+        promo2x1 = director.build(builder, "Celular + Audífonos 2x1");
     }
 
     private void crearPromocionVolumen() {
+        Mouse mouse = new Mouse("Logitech M170", 40000, true);
+
         PromocionBuilder builder = new PromocionVolumenBuilder(mouse, 10, 0.20);
-        promo2 = director.build(builder, "Descuento por volumen Mouse");
+        promoVolumen = director.build(builder, "Descuento por volumen Mouse");
     }
 
     private void crearPromocionCombo() {
+        Laptop laptop = new Laptop("ThinkPad E14", 3000000, "i5", 16, 512, "Windows", null);
+        Mouse mouse = new Mouse("Logitech M170", 40000, true);
+        Funda funda = new Funda("Funda 14 pulgadas", 50000, "Laptop");
+        MemoriaUsb usb = new MemoriaUsb("Kingston 64GB", 30000, 64);
+        Audifono audifono = new Audifono("AirBuds", 150000, true, 20);
+
         PromocionBuilder builder = new PromocionComboBuilder(
                 Arrays.asList(laptop, mouse, funda, usb, audifono),
                 3200000
         );
-        promo3 = director.build(builder, "Combo Laptop + Accesorios");
+
+        promoCombo = director.build(builder, "Combo Laptop + Accesorios");
     }
 
-    private void mostrarInformacion() {
-        mostrarPromocion(promo1, 1);
-        mostrarPromocion(promo2, 50);
-        mostrarPromocion(promo3, 1);
+    private void mostrarInfo2x1() {
+        System.out.println(promo2x1.getNombre()
+                + " -> precio final: "
+                + promo2x1.calcularPrecioFinal(1));
     }
 
-    private void mostrarPromocion(Promocion promocion, int cantidad) {
-        System.out.println(promocion.getNombre()
-                + ": precio final (" + cantidad + " unidades) = "
-                + promocion.calcularPrecioFinal(cantidad));
+    private void mostrarInfoVolumen() {
+        System.out.println(promoVolumen.getNombre()
+                + " -> precio final (50 unidades): "
+                + promoVolumen.calcularPrecioFinal(50));
+    }
+
+    private void mostrarInfoCombo() {
+        System.out.println(promoCombo.getNombre()
+                + " -> precio final: "
+                + promoCombo.calcularPrecioFinal(1));
     }
 }
